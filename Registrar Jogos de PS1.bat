ca6c1369cb1ab8808205ec2b80e15739e63608e7
JoaoRomeuMirandaMartins <joao.romeu.miranda.martins@gmail.com>
2021-11-29 05:14:39 -0300

    Create Registrar Jogos de PS1.bat

---
+++ Registrar Jogos de PS1.bat
@@ -0,0 +1,9 @@
+for /r %%p in (*) do (
+ if %%~xp == .VCD (
+  if not exist "..\APPS\%%~np\" (
+   mkdir "..\APPS\%%~np\"
+   (echo title=%%~np && echo boot=SB.%%~np.ELF) > ..\APPS\%%~np\title.cfg
+   mklink /H "..\APPS\%%~np\SB.%%~np.ELF" POPSTARTER.ELF
+  ) 
+ )
+)
